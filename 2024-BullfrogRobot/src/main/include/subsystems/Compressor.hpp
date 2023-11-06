a1156bfb0e6f58bbbea763e0975f4576785cf796
NO-skcaj <126780741+NO-skcaj@users.noreply.github.com>
2023-11-06 16:13:11 -0500

    Add files via upload

---
+++ 2024-BullfrogRobot/src/main/include/subsystems/Compressor.hpp
@@ -0,0 +1,17 @@
+#ifndef AIR_H
+#define AIR_H
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc/PneumaticHub.h>
+#include "../Constants.h"
+
+
+class Compressor : frc2::SubsystemBase
+{
+    public:
+        void toggle_compressor();
+    private:
+        bool toggle = false;
+};
+
+#endif
